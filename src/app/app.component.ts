0c1932ad754333125afe3abc5b9008c82505bf52
estefa942 <estefany.muriel@udea.edu.co>
2019-08-24 09:24:47 -0500

    convertir a romanos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  public number;
+  public salida;
+  decimal= [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  romano= ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -23,5 +27,17 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  toRomano() {
+    let number = this.number;
+    this.salida = '';
+    let i;
+    for (i = 12; i >= 0; i--) {
+      while (number >= this.decimal[i]) {
+        number = number - this.decimal[i];
+        this.salida = this.salida + this.romano[i];
+      }
+    }
   }
 }
